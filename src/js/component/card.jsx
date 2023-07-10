0218a58a7c67f33ad953da160b19f30e7c56bb0a
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-07-10 19:49:14 +0000

    first commit

---
+++ src/js/component/card.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+
+function Card (){
+ return     <div className="d-flex justify-content-center align-items-center" >
+ <div className="card p-2" style={{ width: '18rem' }}>
+   <img src="https://placekitten.com/667" className="card-img-top" alt="..." />
+   <div className="card-body">
+     <h5 className="card-title">Card title</h5>
+     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+     <a href="#" className="btn btn-primary">Go somewhere</a>
+   </div>
+ </div>
+
+ <div className="card p-2" style={{ width: '18rem' }}>
+   <img src="https://placekitten.com/662" className="card-img-top" alt="..." />
+   <div className="card-body">
+     <h5 className="card-title">Card title</h5>
+     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+     <a href="#" className="btn btn-primary">Go somewhere</a>
+   </div>
+   </div>
+  
+   <div className="card p-2" style={{ width: '18rem' }}>
+   <img src="https://placekitten.com/644" className="card-img-top" alt="..." />
+   <div className="card-body">
+     <h5 className="card-title">Card title</h5>
+     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+     <a href="#" className="btn btn-primary">Go somewhere</a>
+   </div>
+   </div>
+  
+   <div className="card p-2" style={{ width: '18rem' }}>
+   <img src="https://placekitten.com/622" className="card-img-top" alt="..." />
+   <div className="card-body">
+     <h5 className="card-title">Card title</h5>
+     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+     <a href="#" className="btn btn-primary">Go somewhere</a>
+   </div>
+   </div>
+  
+  
+  
+ 
+</div>
+
+}
+
+export default Card
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+function Footer (){
+ return <div class="card-footer justify-content-center d-flex align-items-end">
+ Copyright @ your website 2023
+</div>
+}
+export default Footer
